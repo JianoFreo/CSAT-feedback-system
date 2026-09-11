194b92bb18dc519d4798bf4247d110b14f17407f
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-09-11 15:37:35 +0800

    Refactor health check route endpoint

--- backend/src/routes/healthCheck.route.ts
+++ backend/src/routes/healthCheck.route.ts
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/", (_req, res) => {
   res.status(200).json({ message: "CSAT feedback API is running" });
 });
-router.get("test1", (_req, res) => {
+router.get("/test1", (_req, res) => {
   res.status(200).json({ message: "CSAT feedback API is running test1" });
 });
 export default router;
